ffca76ed442e5c73fcc42f0d13a6b541429e42f3
rayner <rayner.kayo.tec@gmail.com>
2016-05-29 11:25:18 -0300

    #Correção

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,4 +1,4 @@
-	package com.algaworks.cobranca.controller;
+package com.algaworks.cobranca.controller;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,60 +8,87 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
+import com.algaworks.cobranca.service.CadastroTituloService;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
-	//Para criar uma instancia de Titulos
+
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	private static final String PESQUISA_VIEW = "PesquisaTitulo";
+
+	// Para criar uma instancia de Titulos
 	@Autowired
 	private Titulos titulos;
+	
+	@Autowired
+	private CadastroTituloService cadastroTituloService;
 
-	//new Titulo, é para passar o objeto para a view
+	// new Titulo, é para passar o objeto para a view
 	@RequestMapping("/novo")
-	public ModelAndView novo(){
-		ModelAndView modelAndView = new ModelAndView("CadastroTitulo");
+	public ModelAndView novo() {
+		ModelAndView modelAndView = new ModelAndView(CADASTRO_VIEW);
 		modelAndView.addObject(new Titulo());
 		return modelAndView;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors){
-		ModelAndView modelAndView = new ModelAndView("CadastroTitulo");
-		
-		if(errors.hasErrors()){
-			return modelAndView;
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+
+		if (errors.hasErrors()) {
+			return CADASTRO_VIEW;
 		}
-		
-		titulos.save(titulo);
-		modelAndView.addObject("mensagem", "Título salvo com sucesso!");
-		return modelAndView;
+		try {
+			cadastroTituloService.salvar(titulo);
+			attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+			return "redirect:/titulos/novo";
+			// redirect faz uma nova requisição.
+			// DataIntegrityViolationException é quando tem uma volação de integridade no banco
+		} catch (IllegalArgumentException excption) {
+			errors.reject("dataVencimento", null, excption.getMessage());
+			return CADASTRO_VIEW;
+		}
+
 	}
-	
+
 	@RequestMapping
-	public ModelAndView pesquisar(){
+	public ModelAndView pesquisar() {
 		List<Titulo> todosTitulos = titulos.findAll();
-		ModelAndView modelAndView = new ModelAndView("PesquisaTitulo");
+		ModelAndView modelAndView = new ModelAndView(PESQUISA_VIEW);
 		modelAndView.addObject("titulos", todosTitulos);
 		return modelAndView;
 	}
-	
-	//para listar os status do enum, dinamicamente.
-	//essa string é que deve ser chamada na view
+
+	// para listar os status do enum, dinamicamente.
+	// essa string é que deve ser chamada na view
 	@ModelAttribute("todosStatusTitulo")
-	public List<StatusTitulo> todosStatusTitulos(){
+	public List<StatusTitulo> todosStatusTitulos() {
 		return Arrays.asList(StatusTitulo.values());
 	}
-	
-	
+
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		// Spring pega o codigo referente ao objeto titulo, pq estou utilizando
+		// o JPA repository
+		ModelAndView modelAndView = new ModelAndView(CADASTRO_VIEW);
+		modelAndView.addObject(titulo);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+		cadastroTituloService.excluir(codigo);
+		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+		return "redirect:/titulos";
+	}
+
 }
-
-
-
