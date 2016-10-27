569bf53c4c0f7a63118ea3f7203041a0f6151b07
rayner <rayner.kayo.tec@gmail.com>
2016-10-26 21:55:04 -0300

    Realizando deploy com Mavem closes #5

--- cobranca/src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ cobranca/src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -4,6 +4,8 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -12,10 +14,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
-public class CobrancaApplication {
+public class CobrancaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CobrancaApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CobrancaApplication.class);
 	}
 	
 	@Bean
