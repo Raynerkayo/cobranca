568e6cc30dcaf1e5cf8bb7753606995e87107d7d
Kayo Rayner <rayner.kayo.tec@gmail.com>
2016-11-17 07:43:24 -0300

    Update CobrancaApplicationTests.java
    Teste voltar a funcionar.

--- src/test/java/com/algaworks/cobranca/CobrancaApplicationTests.java
+++ src/test/java/com/algaworks/cobranca/CobrancaApplicationTests.java
@@ -100,7 +100,7 @@ public class CobrancaApplicationTests {
 		
 		Titulo titulo = service.buscarPorId(42L);
 		
-		assertEquals("JacquelineSS", titulo.getDescricao());
+		assertEquals("Jacqueline", titulo.getDescricao());
 		assertEquals("Recebido", titulo.getStatus().getDescricao());
 		assertEquals(new BigDecimal("52.00"), titulo.getValor());
 	}
